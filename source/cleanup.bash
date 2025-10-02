ab22cf27655fbbb3b1fca771d1f2020b3db3ddc3
Ran Yakumo <David.diepelt@gmx.net>
2025-10-02 19:29:45 +0200

    adding basic infrastructure for creating all documents

---
+++ source/cleanup.bash
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+IFS=$'\n\t' # Strict Internal Field Separator
+
+for file in *; do
+  fileextension=${file##*.}
+  if [[ -f $file && ($fileextension != "dtx" && $fileextension != "ins" && $fileextension != "bash")]]; then
+    #echo $file
+    rm $file
+  fi
+done
